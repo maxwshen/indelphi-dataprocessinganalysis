59ea2b04da1788d0b5317e5a0046add3070def37
Marit Radder <m.e.radder@student.tudelft.nl>
2023-03-27 14:39:21 +0200

    test

--- inDelphi/main.py
+++ inDelphi/main.py
@@ -1,7 +1,9 @@
 import helpers.neural_network as nn
+import helpers.nearest_neighbours as knn
 
 def train_model(data_url, out_place):
     nn_params, nn_2_params = nn.create(data_url, out_place)
+    rate_model, bp_model, normalizer = knn.train()
 
 if __name__ == '__main__':
     train_model('../pickle-data/inDelphi_counts_and_deletion_features.pkl', './cluster/mshen/prj/mmej_figures/out/d2_model/')
